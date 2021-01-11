4529d0c11400a0e04f11f676d7e3194e3e7a7e77
Thiago Thomas <thiagohthomas@gmail.com>
2021-01-11 09:57:24 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -17,22 +17,24 @@ public class OrderDTO implements Serializable {
     private Double longitude;
     private Instant moment;
     private OrderStatus status;
+    private Double total;
 
     private List<ProductDTO> products = new ArrayList<>();
 
     public OrderDTO() {}
 
-    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
         this.id = id;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
         this.moment = moment;
         this.status = status;
+        this.total = total;
     }
 
     public OrderDTO(Order entity) {
-        this(entity.getId(), entity.getAddress(), entity.getLatitude(), entity.getLongitude(), entity.getMoment(), entity.getStatus());
+        this(entity.getId(), entity.getAddress(), entity.getLatitude(), entity.getLongitude(), entity.getMoment(), entity.getStatus(), entity.getTotal());
         products = entity.getProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
     }
 
@@ -82,6 +84,14 @@ public class OrderDTO implements Serializable {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
     }
 
     public List<ProductDTO> getProducts() {
